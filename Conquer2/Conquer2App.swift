b0b36ce177bc4d1d3ab2b0493ad264f99da01096
larry <larrycomb@gmail.com>
2023-02-18 17:19:47 -0800

    test working?

---
+++ Conquer2/Conquer2App.swift
@@ -0,0 +1,17 @@
+//
+//  Conquer2App.swift
+//  Conquer2
+//
+//  Created by LARRY COMBS on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Conquer2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
