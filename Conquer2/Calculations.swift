cab627054310bb63977ad64d06e598f6a8a3b11d
larry <larrycomb@gmail.com>
2023-02-19 19:22:00 -0800

    adding new file

---
+++ Conquer2/Calculations.swift
@@ -0,0 +1,16 @@
+//
+//  Calculations.swift
+//  Conquer2
+//
+//  Created by LARRY COMBS on 2/19/23.
+//
+
+import Foundation
+
+class Calculations {
+    var amount: Int = 0
+    
+    func addAmount(_ newAmount: Int) {
+        amount += newAmount
+    }
+}
